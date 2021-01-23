90b46cffc48c4d6ffec2c288231f7bd4736dbe55
Sweta7 <anvita1982@gmail.com>
2021-01-23 19:10:10 +1300

    Updates

---
+++ precache-manifest.79a84a9222b34281f2de7396120ebfc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d693902296dbd7137d4de89a4fb8ec7",
+    "url": "/newsApp/index.html"
+  },
+  {
+    "revision": "175400aa18aef9b837a7",
+    "url": "/newsApp/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "c642905a4ae4fe135b31",
+    "url": "/newsApp/static/css/main.9671d20f.chunk.css"
+  },
+  {
+    "revision": "175400aa18aef9b837a7",
+    "url": "/newsApp/static/js/2.bee15aa3.chunk.js"
+  },
+  {
+    "revision": "c642905a4ae4fe135b31",
+    "url": "/newsApp/static/js/main.6a2e35e0.chunk.js"
+  },
+  {
+    "revision": "383bbe8e482b562f113d",
+    "url": "/newsApp/static/js/runtime~main.13a567f7.js"
+  }
+]);
